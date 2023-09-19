4e272586ec25c294ece90fbfce1b9d90545d9659
Afaaq <afaaqmajeed123@gmail.com>
2023-09-19 11:14:34 +0530

    removed variables

--- about.php
+++ about.php
@@ -1,28 +1,19 @@
 <?php
-// session_start();
-echo " Welcome {$_SESSION["username"]}";
 
-if (isset($_POST["logout"])) {
-    session_destroy();
-    header("Location: index.php");
+class firstClass
+{
+    public $z, $x, $y;
+    function getSum()
+    {
+        return $this->z = $this->x + $this->y;
+    }
+    function getSub()
+    {
+        return $this->z = $this->x - $this->y;
+    }
 }
+$objSum = new firstClass();
+$objSub = new firstClass();
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="about.php" method="post">
-        <h1>I am an about page</h1> <br>
-        <input type="submit" name="logout" value="Logout">
-    </form>
-
-</body>
-
-</html>
+echo $objSum->getSum() . "<br>";
+echo $objSub->getSub();
--- database.php
+++ database.php
@@ -1,15 +1,12 @@
 <?php
 
-$server="localhost";
-$user="root";
-$password="";
-$name="mydatabase";
+$server = "localhost";
+$user = "root";
+$password = "";
+$name = "mydatabase";
 
 try {
-  $conn= mysqli_connect($server,$user,$password,$name);
-  if($conn){
-    echo "Connection established";
-  }
+    $conn = mysqli_connect($server, $user, $password, $name);
 } catch (mysqli_sql_exception) {
     echo "Couldn't connect!";
 }
